e595614f3b4e33fcd6906037d81cbc7d6facfaa2
Stephan Rasp <raspstephan@gmail.com>
2014-05-27 13:13:42 +0200

    removed numpydoc from extension for readthedocs

--- sphinx_doc/source/conf.py
+++ sphinx_doc/source/conf.py
@@ -38,7 +38,6 @@ extensions = [
     'sphinx.ext.pngmath',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
